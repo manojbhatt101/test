a7fa32b0a3c194470f3887f4027d9f3fc62f03f5
manojBhatt99 <110886764+manojBhatt99@users.noreply.github.com>
2024-08-20 13:07:46 +0530

    some code update

--- React/my-first-react-app/src/App.js
+++ React/my-first-react-app/src/App.js
@@ -1,11 +1,26 @@
-import Gallery from './Gallery.js';
-import Profile from './Profile.js';
+// import Gallery from './Gallery.js';
+// import Profile from './Profile.js';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <div>
+//       <Gallery />
+//       <Profile />
+//     </div>
+//   );
+// }
+
+const today = new Date();
+
+function formatDate(date) {
+  return Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
   return (
-    <div>
-      <Gallery />
-      <Profile />
-    </div>
+    <h1>To Do List for {formatDate(today)}</h1>
   );
 }
